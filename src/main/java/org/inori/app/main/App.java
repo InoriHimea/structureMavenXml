f20e70b32ee535ef410fea69c33daba63232e777
InoriHimea <icarus347@gmail.com>
2018-08-08 23:46:46 +0800

    添加HTML文件解析

--- src/main/java/org/inori/app/main/App.java
+++ src/main/java/org/inori/app/main/App.java
@@ -7,6 +7,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * App
@@ -24,10 +26,46 @@ public class App {
         //System.out.println(text);
 
         try {
-            Document document = Jsoup.connect(MAVEN_CENTRAL_REPOSITORY_URL).get();
-            Elements aTags = document.getElementsByTag("a");
+            Document topDoc = Jsoup.connect(MAVEN_CENTRAL_REPOSITORY_URL).get();
+            Elements aTags = topDoc.getElementsByTag("a");
+
             int size = aTags.size();
             System.out.println(size);
+
+            /**
+             * 读取一级目录
+             */
+            List<String> firstGroupIdList = new LinkedList<String>();
+            List<String> xmlList = new LinkedList<String>();
+            List<String> txtList = new LinkedList<String>();
+            for (Element aTag : aTags) {
+                if (aTag.hasText()) {
+                    String text = aTag.text();
+
+                    if (! text.equals("../") && text.endsWith("/")) {
+                        firstGroupIdList.add(text);
+                    }
+
+                    if (text.endsWith(".xml")) {
+                        xmlList.add(text);
+                    }
+
+                    if (text.endsWith(".txt")) {
+                        txtList.add(text);
+                    }
+                }
+
+            }
+
+            for (String firstGroupId : firstGroupIdList) {
+                Document secondaryDoc = Jsoup.connect(MAVEN_CENTRAL_REPOSITORY_URL + firstGroupId).get();
+
+                Elements secTags = secondaryDoc.getElementsByTag("a");
+
+                for (Element secTag : secTags) {
+
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/org/inori/app/utils/JsoupHtmlUtils.java
@@ -0,0 +1,12 @@
+package org.inori.app.utils;
+
+/**
+ * 所有相关HTML操作的Jsoup方法
+ * @author InoriHimea
+ * @date 2018/8/8
+ * @since 1.8
+ * @version 1.0
+ */
+public class JsoupHtmlUtils {
+
+}
